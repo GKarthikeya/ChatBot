c878cf557b8c7221dfbf74062ecacf31ed6b453c
GKarthikeya <128343681+GKarthikeya@users.noreply.github.com>
2025-07-15 20:21:49 +0530

    Create build.sh

---
+++ build.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+# Install Chrome
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+apt-get update
+apt-get install -y ./google-chrome-stable_current_amd64.deb
+
+# Install ChromeDriver (automatically handled by webdriver-manager)
